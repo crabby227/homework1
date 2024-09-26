2d1b98696f7f2885b7698881b10f898e049bf396
crabik <polyarusli228@gmail.com>
2024-09-26 11:00:10 +0300

    program2.cpp

--- program2.cpp
+++ program2.cpp
@@ -12,13 +12,14 @@ int main(){
     cin >> V;
     cout<< "a = \t";
     cin >> a;
-
-    T = (V * sin(a)/g) * (1 + sqrt(2*g*H/pow(V,2)*pow(sin(a), 2)));
-    x = V*T*cos(a);
-    y = H + V*T*sin(a) - 0.5*g*pow(T,2);
-
-    cout<< "x = "<< x << endl;
-    cout<< "y = "<< y << endl;
+    if((1 + (2*g*H/pow(V,2)*pow(sin(a),2))) >= 0){
+        T = (V * sin(a)/g) * (1 + sqrt(2*g*H/pow(V,2)*pow(sin(a), 2)));
+        x = V*T*cos(a);
+        y = H + V*T*sin(a) - 0.5*g*pow(T,2);
+    
+        cout<< "x = "<< x << endl;
+        cout<< "y = "<< y << endl;
+    }
     return 0;
     
 }
